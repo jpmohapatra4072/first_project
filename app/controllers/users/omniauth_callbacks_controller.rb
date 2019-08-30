cdfbffb416d036c451b15a0792d3528e3bdfef0b
jyoti prakash <jyotiprakashmohapatra9@gmail.com>
2019-08-30 19:26:23 +0530

    for message

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,14 +3,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env['omniauth.auth'])
-
+    puts "==================================================="
     if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      puts "user present"
+      if @user.provider
+        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      else
+        flash[:notice] = "The email is already registered, you can reset password!!"
+      end
       sign_in_and_redirect @user, event: :authentication
     else
+      puts "user not present"
       session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
+    puts "==================================================="
   end
 
   def facebook
@@ -19,8 +26,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     puts "==================================================="
     if @user.persisted?
       puts "user present"
+      if @user.provider
+        set_flash_message(:notice, :success, kind: "Facebook")
+      else
+        flash[:notice] = "The email is already registered, you can reset password!!" 
+      end
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
       puts "user not present"
       session["devise.facebook_data"] = request.env["omniauth.auth"]
