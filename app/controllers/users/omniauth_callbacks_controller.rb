618c8910a36a66234b9f0d99b28e5ce6916835a2
jyoti prakash <jyotiprakashmohapatra9@gmail.com>
2019-08-30 18:27:15 +0530

    printing log

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -16,14 +16,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
+    puts "==================================================="
     if @user.persisted?
+      puts "user present"
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
+      puts "user not present"
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
+    puts "==================================================="
   end
 
   def failure
