64dcbc1cac6635c9c286676ba65b5235b617d83a
jyoti prakash <jyotiprakashmohapatra9@gmail.com>
2019-08-30 18:22:48 +0530

    for mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'protected-falls-57926.herokuapp.com', :protocol => 'http'}
 
   ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
   ActionMailer::Base.smtp_settings = {
